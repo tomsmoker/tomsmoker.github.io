8f403e0b98fd6f8a133fe46b71630f8ccd71aee0
Thomas Smoker <tomsmoker@gmail.com>
2015-09-20 14:41:46 +0800

    feat: added Perth Coffee Map to Github Pages
    - Making the Perth Coffee Map that I made live

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# tomsmoker.github.io
+# Perth Coffee
+The fifth project for the Udacity Front End Nanodegree
+
+An online display of some coffeeshops I like, along with my favourite coffee they do.
+
+TO RUN:
+Download file and open index.html
+
+
---
+++ js/model.js
@@ -0,0 +1,75 @@
+
+var coffeeShops = [{
+    name: "Lowdown",
+    address: "16a Cloisters Arcade, Parmelia House, 865 Hay St, Perth WA 6000",
+    fave: "Long Mac",
+    lat: -31.9530869,
+    lng: 115.8533204,
+    tag: 'lowdowncoffee'
+}, {
+    name: "Small Print",
+    address: "125 St Georges Terrace, Perth WA 6000",
+    fave: "Long Black",
+    lat: -31.954965,
+    lng: 115.854671,
+    tag: 'smallprintcoffee'
+}, {
+    name: "Standing Room Only",
+    address: "Shop 7, Piccadilly Arcade,Perth WA 6000",
+    fave: "Long Mac",
+    lat: -31.9528473,
+    lng: 115.8550868,
+    tag: 'standingroomonlyperth'
+}, {
+    name: "Velvet Espresso",
+    address: "172 St Georges Terrace, Perth WA 6000",
+    fave: "Long Mac",
+    lat: -31.9537116,
+    lng: 115.8543291,
+    tag: 'velvetespresso'
+}, {
+    name: "Ristretto Espresso",
+    address: "160 St Georges Terrace, Perth WA 6000",
+    fave: "Double Espresso",
+    lat: -31.954074,
+    lng: 115.855036,
+    tag: 'ristrettoperth'
+}, {
+    name: "Driphouse",
+    address: "772A Hay St, Perth WA 6000",
+    fave: "Long Mac",
+    lat: -31.95314,
+    lng: 115.8562336,
+    tag: 'driphouse'
+}, {
+    name: "Moana Coffee",
+    address: "618 Hay St, Perth WA 6000",
+    fave: "Long Mac",
+    lat: -31.954314,
+    lng: 115.8600435,
+    tag: 'moanacoffee'
+}, {
+    name: "Willie Wagtail",
+    address: "131 William Street, Perth WA 6000",
+    fave: "Long Black",
+    lat: -31.951648,
+    lng: 115.8578971,
+    tag: 'williewagtailcoffee'
+}, {
+    name: "Voltage Espresso",
+    address: "12/50 St Georges Terrace, Perth WA 6000",
+    fave: "Long Black",
+    lat: -31.955158,
+    lng: 115.859048,
+    tag: 'voltageespresso'
+}, {
+    name: 'Cheeky Sparrow',
+    address: "317 Murray St, Perth WA 6000",
+    fave: "Long Mac",
+    lat: -31.9523251,
+    lng: 115.8560175,
+    tag: 'cheekysparrow'
+}/*, {
+
+    The standard
+}*/];
